b38886935f44cfac3544095f8fbb7210a149ce3b
chukwuebuka Anyadiegwu <chubukass@gmail.com>
2021-09-17 12:13:13 +0100

    feat(post model): create the datABASE model for the post api

---
+++ models/Posts.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    postMessage: {
+      type: String,
+      required: [true, "Post massage is needed"],
+    },
+    comments: [
+      {
+        type: String,
+        ref: "Comments",
+      },
+    ],
+    totalComments: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Posts", postSchema);
+
+module.exports = Post;
