76d827f5d519e41926862ae00793070823c78ab2
chukwuebuka Anyadiegwu <chubukass@gmail.com>
2021-09-17 12:44:32 +0100

    feat(post controller): create a `Create` function in the post controller
    The create function handles the creation of a new post

---
+++ controllers/postsController.js
@@ -0,0 +1,34 @@
+const Post = require("../models/Post");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../middleware/appError");
+const validateInputs = require("../utils/validateInputs");
+const appSuccess = require("../utils/appSuccess");
+const filesUpload = require("../utils/filesUpload");
+const cloudinary = require("../utils/cloudinary");
+
+// CREATE POST
+exports.createPost = catchAsync(async (req, res, next) => {
+  // CREATE THE NEW POST
+  let data = await new Post({
+    ...req.body,
+  });
+
+  // CHECK IF FILES ARE IN THE POST
+  if (!req.files) {
+    data = await data.save(); // SAVE THE POST
+  } else {
+    // CHECK IF FILES ARE MORE THAN TWO
+    if (req.files.postPictures.length > 2)
+      return next(new AppError("You can not upload more than two files!", 400));
+    // SAVE THE FILES TO CLOUDINARY AND SAVE ITS RETURN IT URL AND ID
+    const postPictures = await filesUpload(req.files.postPictures, next);
+
+    if (typeof postPictures === "string")
+      return next(new AppError(postPictures, 404)); // check if there is error and return it
+
+    data.postPictures = postPictures; // ATTACH THE RETURED ARRAY TO THE POST
+    data = await data.save(); // SAVE THE POST
+  }
+
+  appSuccess(200, "Posts created successfully", res, data);
+});
